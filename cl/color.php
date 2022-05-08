87c06f7e36b0945a8179e03ad63f95359675040d
SafrullahMohomed <emails3msm@gmail.com>
2022-05-08 12:28:38 +0530

    added comments to the code

--- cl/color.php
+++ cl/color.php
@@ -11,6 +11,7 @@
 </head>
 
 <body>
+    <!-- paste your code here  -->
     <div class="sMsk rClear">
         <div class="r1Zeile rInf rKln" id="sEig" style="min-height: 1em;">
             <div class="rLinks"><span title="Vocabulary Certificate A1"><b>A1</b></span> · <span
@@ -23,6 +24,7 @@
             onkeydown="App.pupAus(event.target, true)" title=""
             aria-placeholder="Enter German sentence..."><mark id="xeaf57" data-mrefs="" title="!!!satz/satzglied.!!!" class="sSpc"></mark><mark id="x880a8" data-mrefs="" onclick="App.pupToggle(event,this,-1661038893)" title="Subject" class="sSbj">Das</mark><mark id="xc0260" data-mrefs="" title="!!!satz/satzglied.!!!" class="sSpc"> </mark><mark id="x64f80" data-mrefs="" onclick="App.pupToggle(event,this,76267754)" title="Predicate" class="sFin">war</mark><mark id="x434c6" data-mrefs="" title="!!!satz/satzglied.!!!" class="sSpc"> </mark><mark id="x9eb9d" data-mrefs="" onclick="App.pupToggle(event,this,25664053)" title="Adverbial" class="sAdv">ja</mark><mark id="xaf1bb" data-mrefs="" title="!!!satz/satzglied.!!!" class="sSpc"> </mark><mark id="xc6b49" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sPrv">so</mark><mark id="x5d3dc" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sSpc sPrv"> </mark><mark id="x9a086" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sPrv">was</mark><mark id="x56418" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sSpc sPrv"> </mark><mark id="xcf5a3" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sPrv">von</mark><mark id="x44912" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sSpc sPrv"> </mark><mark id="xa5e24" data-mrefs="" onclick="App.pupToggle(event,this,-1659015849)" title="Predicative" class="sPrv">klar</mark><mark id="x42003" data-mrefs="" title="!!!satz/satzglied.!!!" class="sSpc"></mark><mark id="x66077" data-mrefs="" title="!!!satz/satzglied.!!!" class="">.</mark><mark id="xf0a6c" data-mrefs="" title="!!!satz/satzglied.!!!" class="sSpc"></mark></pre>
     </div>
+    <!-- you can paste another code here to append or you can delete previos and update it anyhow put your code inside body tag-->
 </body>
 
 </html>
